63846fbb6e3410d1c5084fc6cb5904182273eee6
kathmuri <kathmuri@mobiquityinc.com>
2021-12-27 20:52:38 +0530

    Initial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,35 @@
+//Write swift comments for single line with double slash symbol
+
+//If you want print any data on console- print
+print("Hi to all")
+
+//swift variables
+var userName = "John"
+//declare variables
+var siteName: String // string type
+var id: Int  // int type
+//assign values to variables
+siteName = "google.com"
+print(siteName)
+// declare variables and assign the value to variables at a time
+var carName = "WWW"
+print(carName)
+//change the value of the variable
+carName = "Honda"
+print(carName)
+
+//rules for naming swift variables
+//--> start with either a letter, an underscore _
+var withVar: String
+var _withUnderScoreVar: String
+//-->Variable names cannot start with numbers and dollar sign $
+//-->Swift is case-sensitive. So A and a are different variables
+var name = "Apple"
+var Name = "banana"
+print(name)
+print(Name)
+
+//Note:
+//-->It's a good practice to give a descriptive variable name.
+//-->In Swift, variable names are generally written in camelCase if they have multiple words.
+
