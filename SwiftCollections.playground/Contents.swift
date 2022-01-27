88d4d967ae8b651937011c3a738c6c45c52e953d
Kavya Athmuri <kathmuri@mobiquityinc.com>
2022-01-27 08:05:15 +0530

    Add swift collection - sets

--- SwiftCollections.playground/Contents.swift
+++ SwiftCollections.playground/Contents.swift
@@ -94,3 +94,98 @@ print("The empty array is empty?: \(emptyArray.isEmpty)")
 var mixedDataArrayOfStudentInfo :[Any] = ["Jhon",29, 30.00]
 print(mixedDataArrayOfStudentInfo)
 //Here, [Any] specifies that address can hold elements of any data type. In this case, it stores both String, Integer and Float data.
+
+/*
+ Swift sets:
+ A set is a collection of unique data. That is, elements of a set cannot be duplicate.
+ */
+var stundentIds : Set = [100,20,17,20,44,44]
+print("Student id's are \(stundentIds)")
+/*
+ Note: When you run this code, you might get output in a different order. This is because the set has no particular order.
+ 
+ Add Elements to a Set:
+ We use the insert() method to add the specified element to a set.
+ */
+stundentIds.insert(33)
+print("Student id's after added one new element is \(stundentIds)")
+/*
+ Remove an Element from a Set:
+ We use the remove() method to remove the specified element from a set.
+ */
+stundentIds.remove(44)
+print("Student is's after remove the one element is \(stundentIds)")
+/*
+ Other Set Methods
+ Method                     Description
+ sorted()                   sorts set elements
+ forEach()                  performs the specified actions on                         each element
+ contains()                 searches the specified element in                         a set
+ randomElement()            returns a random element from the                         set
+ firstIndex()               returns the index of the given                            element
+ removeFirst()              to remove the first element of a                          set
+ removeAll()                to remove all elements of a set
+ 
+ Iterate Over a Set:
+ We can use the for loop to iterate over the elements of a set.
+ */
+
+for id in stundentIds {
+    print(id,terminator: "\t")
+}
+
+/*
+ Find Number of Set Elements
+ We can use the count property to find the number of elements present in a Set.
+ */
+print("Student id's count is \(stundentIds.count)")
+/*
+ Swift Set Operations:
+ Swift Set provides different built-in methods to perform mathematical set operations like union, intersection, subtraction, and symmetric difference.
+
+ 1. Union of Two Sets:
+ The union of two sets A and B include all the elements of set A and B.
+ */
+let set1:Set<String> = Set(["Gova","Banana","Mango"])
+let set2:Set<String> = Set(["Sithapal","Banana","Gova"])
+print("Union: ",set1.union(set2))
+/*
+ 2. Intersection between Two Sets:
+ The intersection of two sets A and B include the common elements between set A and B.
+ */
+print("Intersections: ",set1.intersection(set2))
+/*
+ 3. Difference between Two Sets:
+ The difference between two sets A and B include elements of set A that are not present on set B.
+ */
+print("Substraction: ",set1.subtracting(set2))
+/*
+ Note: setA.substracting(setB) is equivalent to A - B set operation.
+
+ 4. Symmetric Difference between Two Sets:
+ The symmetric difference between two sets A and B includes all elements of A and B without the common elements.
+ */
+print("Symmetric difference: ",set1.symmetricDifference(set2))
+/*
+ 5. Check Subset of a Set:
+ Set B is said to be the subset of set A if all elements of B are also present in A.
+ */
+print("Subset: ",set1.isSubset(of: set2))
+/*
+ Check if two sets are equal
+ We can use the == operator to check whether two sets are equal or not.
+ */
+if set1 == set2 {
+    print("Two sets values are equal")
+} else {
+    print("Two sets values are not equal")
+}
+/*
+ Create an Empty Set:
+ In Swift, we can also create an empty set.
+ It is important to note that, while creating an empty set, we must specify the data type inside the <> followed by an initializer syntax ().
+
+ Here, <Int>() specifies that the empty set can only store integer data elements.
+ */
+let emptySet = Set<String>()
+print(emptySet)
