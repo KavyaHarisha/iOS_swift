98d421876cdd0785e50aa424c6c4f1d7950cdf16
Kavya Athmuri <kathmuri@mobiquityinc.com>
2022-02-06 07:54:23 +0530

    Swift more information for functions and nested functions

--- SwiftFunctions.playground/Contents.swift
+++ SwiftFunctions.playground/Contents.swift
@@ -73,3 +73,169 @@ print("Power of the the number is",pow(4, 2))
  1. Code Reusable - We can use the same function multiple times in our program which makes our code reusable.
  2. Code Readability - Functions help us break our code into chunks to make our program readable and easy to understand.
  */
+
+/*
+ Function Parameter with Default Values:
+ We can provide default values to function parameters.We use the = operator to provide default values.
+ */
+func subtractNumber(num1: Int = 12, num2: Int = 4) -> Int {
+    return num1 - num2
+}
+print("Subtraction with provided first value:",subtractNumber(num1: 20))
+print("Subtraction with default values:",subtractNumber())
+print("Subtraction with provide two values:",subtractNumber(num1: 13, num2: 5))
+/*
+ Here, we have provided default values 12 and 4 for parameters num1 and num2 respectively. Here's how this program works
+
+ 1. subtractNumber(num1: 13, num2: 5)
+ Both values are passed during the function call. Hence, these values are used instead of the default values.
+
+ 2. subtractNumber(num1: 20)
+ Only one the value for parameter num1 is passed during the function call. So, the default value is used for parameter num2.
+
+ 3. subtractNumber()
+ No value is passed during the function call. Hence, default value is used for both parameters num1 and num2.
+ 
+ Function With Argument Label:
+ We can use the argument labels to define a function in an expressive and sentence-like manner.
+ */
+func multiply(of a: Int = 10, and b: Int = 4) -> Int{
+    return a * b
+}
+print("Function of multiply with argument labels :",multiply(of: 20, and: 2))
+/*
+ Here, the multiply() function has argument labels: of and and. While calling a function, we can use the argument label instead of parameter names.
+ 
+ Omit Argument Labels:
+ We can also omit the argument label by writing a _ before the parameter name.
+ */
+func division(_ a: Int, _ b: Int)-> Int {
+    return a/b
+}
+print("Function of division with omit argument label:",division(45, 5))
+/*
+ Note: If we use _ before the parameter name, then we can call the function without an argument label or parameter name.
+ 
+ Function with variadic parameters:
+ Sometimes, we do not know in advance the number of arguments that will be passed into a function. To handle this kind of situation, we can use variadic parameters in Swift.
+ Variadic parameters allow us to pass a varying number of values during a function call.
+ We use the ... character after the parameter's type to denote the variadic parameters.
+ */
+func sum(numbers: Int...) ->Int {
+    var sumResult = 0
+    for num in numbers {
+        sumResult = sumResult + num
+    }
+    return sumResult
+}
+print("Function of variadic parameters of sum:",sum(numbers: 1,2,3,4))
+/*
+ Note: After getting multiple values, numbers behave as an array so we are able to use the for loop to access each value.
+ Function With inout Parameters:
+ When we define a function parameter, the function parameter cannot be modified inside the function body.
+ func changeName(name: String) {
+   if name == ""{
+     // Cause Error
+     name = "Ross"
+   }
+ }
+ Here, the above code will generate an error: Error: cannot assign to value: 'name' is a 'let' constant. This is because the parameter behaves as a constant value.
+
+ To overcome this, we need to define the parameter as an inout parameter.
+ */
+func changeName(name: inout String) {
+    if name == "Rose" {
+        name = "Josh"
+    }
+}
+var userName = "Rose"
+print("Before: \(userName)")
+changeName(name: &userName)
+print("After:",userName)
+/*
+ In the above example, we have declared a function changeName() that accepts inout parameter name.
+
+ While calling a function with inout parameter, we must use the ampersand(&) sign before a variable name passed as an argument.
+ changeName(name: &userName)
+ Behind the scenes, an in-out parameter has a value that is passed into the function, is modified by the function, and is passed back out of the function to replace the original value. Therefore the value passed in the function call must be a variable.
+ 
+ Function with Return Multiple Values:
+ A function can also return multiple values.
+ func checkMarks() -> (String, Int) {
+   ...
+   return (message, marks)
+ }
+ Here, the return statement returns two values: message and marks.
+ Also, -> (String, Int) specifies the return type message and marks. And, the order should be the same. That is, the first returned value should be a string and second should be an integer.
+ */
+func compute(_ num: Int) -> (Int, Int, Int) {
+    let square = num * num
+    let cube = square * num
+    return (num, square, cube)
+}
+let computeResult = compute(10)
+print("Compute number:",computeResult.0)
+print("Compute square:",computeResult.1)
+print("Compute cube:",computeResult.2)
+/*
+ Here, the function is returning the number, its square and the cube value at once.The returned values are stored in result. Here, result is a tuple that stores multiple values.
+To access individual values from a tuple, we use the index number. That's why result.0 returns 10, result.1 returns 100, and results.2 returns 1000.
+ 
+ Swift Nested Functions:
+ In Swift, a function can exist inside the body of another function. This is called a nested function.
+ Syntax of Nested Function:
+ // outer function
+ func function1() {
+   // code
+
+   // inner function
+   func function2() {
+     // code
+   }
+ }
+ Here, the function function2() is nested inside the outer function function1()
+ */
+func addNumbersWithNested() {
+    print("Addition")
+    func displayAddition (_ a: Int,_ b : Int) {
+        print("a+b value is \(a+b)")
+    }
+    displayAddition(11, 20)
+}
+addNumbersWithNested()
+/*
+ In the above example, the function displayAddition() is nested inside the function addNumbersWithNested().
+ Note: If we try to call the inner function from outside of the outer function, we'll get the error message: use of unresolved identifier.
+ */
+//Nested Function with Return Values
+func operate(symbol: String) -> (Int, Int) -> Int {
+    func add(_ a: Int,_ b:Int) -> Int {
+        return a + b
+    }
+    
+    func subtract(num1:Int, num2:Int) -> Int {
+        return num1 - num2
+      }
+    let operation = (symbol == "+") ?  add : subtract
+    return operation
+}
+let operationSubtract = operate(symbol: "-")
+let nestedResult = operationSubtract(5,2)
+print("Result of nested function with return value is:",nestedResult)
+/*
+ In the above example, the functions add() and subtract() are nested inside the operate() function . Notice the declaration of the outer function
+
+ func operate(symbol: String) -> (Int, Int) -> Int {
+   ...
+ }
+ Here, the return type (Int, Int) -> Int specifies that the outer function returns a function with
+
+ -> two parameters of Int type and
+ -> a return value of Int type.
+ Since this matches the function definition of inner functions, the outer function returns one of the inner functions.
+
+ This is why we are able to call the inner function from outside of the outer function as well using
+
+ let result = operation(5, 3)
+ Here, operation(5, 3) is replaced by either add(5, 3) or subtract(5, 3) based on the value of symbol passed to the operate() function.
+ */
